a53fc87e53e5026c91280866d6b17bfb07166042
Randomystick <bryankhleow@gmail.com>
2021-03-15 16:04:02 +0800

    reorganise

--- lab 4/Labs/Lab4/Lab4_Optional_Mic_FFT/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/ti/simplelink_msp432p4_sdk_2_20_00_12/source/ti/grlib/lib/ccs/m4f/grlib.a" -l"C:/ti/simplelink_msp432p4_sdk_2_20_00_12/source/third_party/CMSIS/DSP_Lib/lib/ccs/m4f/arm_cortexM4lf_math.a" -l"ti/display/lib/display.aem4f" -l"ti/grlib/lib/ccs/m4f/grlib.a" -l"third_party/spiffs/lib/ccs/m4f/spiffs.a" -l"ti/drivers/lib/drivers_msp432p401x.aem4f" -l"third_party/fatfs/lib/ccs/m4f/fatfs.a" -l"ti/devices/msp432p4xx/driverlib/ccs/msp432p4xx_driverlib.lib" -llibc.a
-
--- lab 4/Labs/Lab4/Lab4_Optional_Temperature/Debug/ccs/startup_msp432p401r_ccs.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-ccs/startup_msp432p401r_ccs.obj: ../ccs/startup_msp432p401r_ccs.c
-ccs/startup_msp432p401r_ccs.obj: C:/ti/ccs740/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
-
-../ccs/startup_msp432p401r_ccs.c: 
-C:/ti/ccs740/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
